aea10051e03335d2b06d06de603c8b80dca3447f
Gaurav Soni <gsoni8158@gmail.com>
2023-05-22 02:05:32 +0530

    submitting code-Mon May 22 2023 02:05:32 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,31 @@
-
-import React, { useState } from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-import Child from "./child";
 
 const App = () => {
-  const [data,setData]=useState("");
-  
-  const setOption1 = () => setData(1);
-  const setOption2 = () => setData(2);
+
+  const [data, setdata] = useState("");
+
+
   return (
-    <div>
-      <div className="parent">
-      <h1>Parent Component</h1>
-     <div className="child1">
-     <h1>Child Component 1</h1>
-     <button onClick={setOption1}>Option 1</button>
-     </div>
-     <div className="child1">
-     <h1>Child Component 2</h1>
-     <button onClick={setOption2}>Option 2</button>
-     </div>
+    <div className="parent">
+    <h1>Parent Component</h1>
+    <div className="tomato-child">
+      <h2>Child Component 1</h2>
+      <button onClick={(e)=>{setdata(e.target.innerHTML)}}>Option 1</button>
     </div>
-    <p>Selected Option: {data}</p>
+
+    <div className="yellow-child">
+      <h2>Child Component 2</h2>
+      <button onClick={(e)=>{setdata(e.target.innerHTML)}}>Option 2</button>
     </div>
-    
+
+    <p className="answer">
+    Selected Option: {data}
+
+    </p>
+        
+    </div>
   )
 }
 
-export default App;
+export default App
