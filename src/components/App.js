6ec2527afb704dd2026bf951c58570d585ebf8c0
Gaurav Soni <gsoni8158@gmail.com>
2023-05-22 00:55:44 +0530

    submitting code-Mon May 22 2023 00:55:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Child from "./child";
 
 const App = () => {
+  const [data,setData]=useState("");
+  
+  const setOption1 = () => setData(1);
+  const setOption2 = () => setData(2);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div className="parent">
+      <h1>Parent Component</h1>
+     <div className="child1">
+     <h1>Child Component 1</h1>
+     <button onClick={setOption1}>Option 1</button>
+     </div>
+     <div className="child1">
+     <h1>Child Component 2</h1>
+     <button onClick={setOption2}>Option 2</button>
+     </div>
     </div>
+    <p>Selected Option: {data}</p>
+    </div>
+    
   )
 }
 
-export default App
+export default App;
